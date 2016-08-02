aadce265a3db5006ec95f3e6cfe1dcc568251666
edorsey007 <edorsey@liveintent.com>
2016-08-02 18:04:12 -0400

    file update

--- week5/class-09-ajax-and-apis/citi_bike/main.js
+++ week5/class-09-ajax-and-apis/citi_bike/main.js
@@ -8,4 +8,56 @@ console.log() if successful.
 */
 
 window.onload = function () {
+
+
+	document.getElementById('getDataButton').onclick = makeRequest
+
+
+	function makeRequest() {
+
+		var url = 'https://gbfs.citibikenyc.com/gbfs/en/station_information.json';
+
+		// 1 create instance of the XHR object
+		// var httpRequest = new XMLHttpRequest();
+
+		// // 2 write a fn to handle the request
+		// httpRequest.onreadystatechange = responseMethod
+
+		// // 3 open the request
+		// httpRequest.open('GET', url);
+
+		// // 4 send the request
+		// httpRequest.send();
+
+		// // request logic
+		// function responseMethod () {
+		// 	// check if DONE
+		// 	if (httpRequest.readyState === XMLHttpRequest.DONE) {
+		// 		// if successful
+		// 		if (httpRequest.status === 200) {
+		// 			console.log(JSON.parse(httpRequest.response));
+		// 		} else {
+		// 			console.log('There was a problem with the request')
+		// 		}
+		// 	}
+		// }
+
+		// $.get(url, function (response) {
+		// 	console.log(response);
+		// })
+
+		$.ajax({
+			url: url,
+			type: 'GET',
+			success: function (response) {
+				console.log(response);
+			},
+			error: function (response) {
+				console.log(response);
+			}
+		})
+
+
+	}
+
 };
